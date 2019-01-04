479efbe9f2bbd5cff60f655b6cd6d590a9a2ca83
nisol91 <nicolasolzi91@gmail.com>
2019-01-04 15:33:12 +0100

    1

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+$(window).scroll(function() {
+  $('.nav').addClass('sopra')
+  $('.nav').addClass('bianco')
+  $('.nav .log_g').addClass('alt_tot')
+
+  $('.top-up img').addClass('sotto')
+});
+
+$('.adress').click(function(event) {
+  $(this).addClass('.adress-i')
+});
+
+
+
+
+});
