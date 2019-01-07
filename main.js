c083423e79a7592304b2e7a717b064f9455e5e80
nisol91 <nicolasolzi91@gmail.com>
2019-01-07 14:42:56 +0100

    1

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 
 $(window).scroll(function() {
-  $('.nav').addClass('sopra')
-  $('.nav').addClass('bianco')
-  $('.nav .log_g').addClass('alt_tot')
+  var height = $(window).scrollTop();
+  console.log(height);
+  if(height  > 0) {
+    $('.nav').addClass('sopra')
+    $('.nav').addClass('bianco')
+    $('.nav .log_g').addClass('alt_tot')
 
-  $('.top-up img').addClass('sotto')
+    $('.top-up img').addClass('sotto')
+  } else if (height === 0) {
+    $('.nav').removeClass('sopra')
+    $('.nav').removeClass('bianco')
+    $('.nav .log_g').removeClass('alt_tot')
+
+    $('.top-up img').removeClass('sotto')
+    $('.nav').addClass('transizione')
+
+  }
 });
+
 
 $('.adress').click(function() {
   $(this).addClass('.adress-i')
@@ -22,6 +35,9 @@ $('.card').mouseleave(function() {
   $('.freccia').addClass('nascosta')
   $('.iniziale').addClass('nascosta')
   $(this).find('img').removeClass('img_zoom')
+  $(this).find('img').addClass('transizione')
+//NB se voglio fare si che ci sia una transizione nell add/remove class di jquery,
+//mi basta aggiungere una classe css che abbia transition come proprieta, esattamente come ho fatto qui sopra.
 });
 
 
